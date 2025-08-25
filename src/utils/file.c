d4cf4b688620e0f16ebaf5ff432a627cafd1fe07
cavallo02 <duallovercouple@gmail.com>
2025-08-25 11:46:32 +0200

    fix: removed fclose on null pointer

--- src/utils/file.c
+++ src/utils/file.c
@@ -4,8 +4,9 @@
 FILE * open_file_read_only( const char * location )
 {
   FILE* fptr;
-
+  printf("Open location: %s\n", location );
   fptr = fopen( location, "r" );
+  printf("Open file: %p\n", ( void * ) fptr);
 
   return fptr;
 }
@@ -14,22 +15,29 @@ file_c * get_html_data( const char * location )
 {
   FILE * fptr;
   file_c * html = ( file_c * ) malloc( sizeof( file_c ) );
+  html->size = 0;
+  printf("IN file: %s\n", location);
 	
   //Counting characters in a file
-  if( ( fptr = open_file_read_only( location ) ) == NULL )
+  fptr = open_file_read_only( location );
+  if( fptr == NULL )
   {
-    html->size = 0;
-    fclose( fptr );
+    printf("File ptr null.\n");
     return html;
   }
+  
   html->size = characters_count( fptr );
+  fseek( fptr, 0, SEEK_SET );
   
   //Copying data
-  char * html_content = ( char * )  malloc( html->size * sizeof( char ) );
+  char * html_content = ( char * )  malloc( ( html->size + 1 ) * sizeof( char ) );
   if( html_content == NULL ) { html->size = 0; return html; }
-  fseek( fptr, 0, SEEK_SET );
-  fgets( html_content, html->size, fptr );
+  size_t bytes_read = fread( html_content, 1, html->size, fptr );
+  printf( "File size: %u\n", bytes_read );
+  html_content[bytes_read] = '\0';
   html->content = html_content;
+  printf( "File content: %s\n", html_content );
+  
   
   return html;
 }
